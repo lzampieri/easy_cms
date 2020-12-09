952c2de99a39269be0463b3a7fa392a79736ef86
lzampieri <zampieri.leonardo98@gmail.com>
2020-12-09 09:28:57 +0100

    Separated Config file

--- admin.php
+++ admin.php
@@ -1,30 +1,6 @@
 <?php
 // Configuration parameters
-// Hashed (sha256) password to access control panel
-$access_password = "15e559ae3df4f3d8cce60e4324880aa84a0325969686a2b42325f722acdbbb9a";
-// Database connection parameters
-$db_server = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_name = "my_galielo";
-// Editable fields. An associative array, in which each key is the name of a table.
-// Each element is itself an associative array, in which each key is the name of an
-// editable field and its value is the shown name, plus the '__name' field with the
-// shown name of the table and the '__unique' field with the name of the unique field
-// in the table
-// As a choice of the author, no removing of rows is available
-$tables = array(
-    "giocatori" => array(
-        "__name" => "Lista giocatori",
-        "__unique" => "ID",
-        "Nome" => "Nome"
-    ),
-    "ccup" => array(
-        "__name" => "Coppe dei Campioni",
-        "__unique" => "ID",
-        "Hidden" => "Nascosto?"
-    )
-);
+require('config.php');
 
 // Bootstrap head
 ?>
